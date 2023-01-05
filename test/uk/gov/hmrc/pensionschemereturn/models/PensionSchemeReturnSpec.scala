945960c8116f2b43c9f173fe30a95a74a308a545
westwater <gmwestwater@hotmail.co.uk>
2023-01-05 15:18:50 +0000

    PSR-40 Added initial data model

---
+++ test/uk/gov/hmrc/pensionschemereturn/models/PensionSchemeReturnSpec.scala
@@ -0,0 +1,13 @@
+package uk.gov.hmrc.pensionschemereturn.models
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class PensionSchemeReturnSpec extends AnyWordSpec with Matchers {
+
+  "PensionSchemeReturn" should {
+    "serialise to json correctly" in {
+
+    }
+  }
+}
