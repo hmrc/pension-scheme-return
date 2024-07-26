5b65d0ce5ec740d98efc2bf2feefe769a0d43f49
kberec <26416940+kberec@users.noreply.github.com>
2024-07-26 11:08:28 +0100

    PSR-1312 Removed mongo config

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,11 +3,9 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapVersion = "9.0.0"
-  private val hmrcMongoVersion = "1.8.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                   %% "bootstrap-backend-play-30"  % bootstrapVersion,
-    "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
     "org.typelevel"                 %% "cats-core"                  % "2.10.0",
     "com.networknt"                 %  "json-schema-validator"      % "1.4.0" exclude ("org.slf4j", "slf4j-api"),
     "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.17.0"
@@ -15,7 +13,6 @@ object AppDependencies {
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                  %% "bootstrap-test-play-30"      % bootstrapVersion,
-    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-test-play-30"     % hmrcMongoVersion,
     "org.scalatestplus"            %% "scalacheck-1-17"             % "3.2.18.0",
     "com.softwaremill.diffx"       %% "diffx-scalatest-should"      % "0.9.0"
   ).map(_ % Test)
