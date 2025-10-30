130ee0533aa3a646265dbaf684a90bca94baae02
MadStu <737342+MadStu@users.noreply.github.com>
2025-10-30 14:06:51 +0000

    [PSR-2148] Dependency Upgrade
    .
    
    .

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.2.0"
+  private val bootstrapVersion = "10.3.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                   %% "bootstrap-backend-play-30"  % bootstrapVersion,
-    "org.typelevel"                 %% "cats-core"                  % "2.12.0",
-    "com.networknt"                 %  "json-schema-validator"      % "1.5.3" exclude ("org.slf4j", "slf4j-api"),
-    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.18.1"
+    "org.typelevel"                 %% "cats-core"                  % "2.13.0",
+    "com.networknt"                 %  "json-schema-validator"      % "1.5.9" exclude ("org.slf4j", "slf4j-api"),
+    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.20.0"
   )
 
   val test: Seq[ModuleID] = Seq(
