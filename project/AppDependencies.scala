4939966e32dc0e53c665ff15c5f6f5ed4b83e68f
MadStu <737342+MadStu@users.noreply.github.com>
2025-12-03 14:10:18 +0000

    [PSR-2156] Update Dependencies non-SIPP BE

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.3.0"
+  private val bootstrapVersion = "10.4.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                   %% "bootstrap-backend-play-30"  % bootstrapVersion,
     "org.typelevel"                 %% "cats-core"                  % "2.13.0",
     "com.networknt"                 %  "json-schema-validator"      % "1.5.9" exclude ("org.slf4j", "slf4j-api"),
-    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.20.0"
+    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.20.1"
   )
 
   val test: Seq[ModuleID] = Seq(
